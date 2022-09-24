a66267e5a47345a588508ea57b351cea86be5726
Alexander Augusto <alexaasf1010@gmail.com>
2022-09-24 14:59:37 -0300

    Fix conflit in next config

--- next.config.js
+++ next.config.js
@@ -7,12 +7,8 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-<<<<<<< Updated upstream
-  assetPrefix: isProd ? './' : undefined,
-=======
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
->>>>>>> Stashed changes
 }
 
 module.exports = nextConfig
